b067ddcd199fa309784146f36176243833828016
ZachCortez <Zach.Cortez@icloud.com>
2025-07-05 23:05:19 -0400

    Fix TypeScript type for categories in Search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,4 +1,4 @@
-import {FlatList, Text, View} from 'react-native'
+import {Button, FlatList, Text, View} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
 import useAppwrite from "@/lib/useAppwrite";
 import {getCategories, getMenu} from "@/lib/appwrite";
@@ -11,6 +11,7 @@ import {MenuItem} from "@/type";
 
 import Filter from "@/components/Filter";
 import SearchBar from "@/components/SearchBar";
+
 
 const Search = () => {
     const { category, query } = useLocalSearchParams<{query: string; category: string}>()
@@ -27,41 +28,34 @@ const Search = () => {
 
     return (
         <SafeAreaView className="bg-white h-full">
-            <FlatList
-                data={data}
-                renderItem={({ item, index }) => {
-                    const isFirstRightColItem = index % 2 === 0;
+            <FlatList data={data} renderItem={({ item, index }) => {
+                const isFirstRightColItem = index % 2 === 0;
 
-                    return (
-                        <View className={cn("flex-1 max-w-[48%]", !isFirstRightColItem ? 'mt-10': 'mt-0')}>
-                            <MenuCard item={item as MenuItem} />
-                        </View>
-                    )
-                }}
-                keyExtractor={item => item.$id}
-                numColumns={2}
-                columnWrapperClassName="gap-7"
-                contentContainerClassName="gap-7 px-5 pb-32"
-                ListHeaderComponent={() => (
-                    <View className="my-5 gap-5">
-                        <View className="flex-between flex-row w-full">
-                            <View className="flex-start">
-                                <Text className="small-bold uppercase text-primary">Search</Text>
-                                <View className="flex-start flex-row gap-x-1 mt-0.5">
-                                    <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
-                                </View>
-                            </View>
-
-                            <CartButton />
-                        </View>
-
-                        <SearchBar />
-
-                        <Filter categories={categories!} />
+                return (
+                    <View className={cn("flex-1 max-w-[48%]", !isFirstRightColItem ? 'mt-10': 'mt-0')}>
+                        <MenuCard item={item as MenuItem} />
                     </View>
-                )}
-                ListEmptyComponent={() => !loading && <Text>No results</Text>}
-            />
+                )
+            }} keyExtractor={item => item.$id} numColumns={2} columnWrapperClassName="gap-7" contentContainerClassName="gap-7 px-5 pb-32" ListHeaderComponent={() => (
+                <View className="my-5 gap-5">
+                    <View className="flex-between flex-row w-full">
+                        <View className="flex-start">
+                            <Text className="small-bold uppercase text-primary">Search</Text>
+                            <View className="flex-start flex-row gap-x-1 mt-0.5">
+                                <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
+                            </View>
+                        </View>
+
+                        <CartButton />
+                    </View>
+
+                    <SearchBar />
+                    <Filter categories={categories}/>
+
+                </View>
+            )} ListEmptyComponent={() => (
+                loading ? <Text className="text-center mt-10">Loading...</Text> : <Text>No results</Text>
+            )}/>
         </SafeAreaView>
     )
 }
